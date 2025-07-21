8a037c46a730a802f1044b2bab191a6422330c5f
Danait Hishe <danait.hishe@hst-et.com>
2025-07-21 11:24:05 +0300

    Fix header case and add null check for Authorization

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -1,19 +1,20 @@
-const jwt = require ('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
-const verifyJWT = (req,res,next) =>{
+const verifyJWT = (req, res, next) => {
     const authHeader = req.headers['authorization'];
-    if(!authHeader?.startsWith('Bearer ')) return res.sendStatus(401);
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.sendStatus(401); // Unauthorized
+    }
+
     const token = authHeader.split(' ')[1];
-    jwt.verify(
-        token,
-        process.env.ACCESS_TOKEN_SECRET,
-        (err, decoded) => {
-            if(err) return res.sendStatus(403);//invalid token
-            req.user = decoded.UserInfo.username;
-            req.roles = decoded.UserInfo.roles;
-            next();
-        }
-    )
-}
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) return res.sendStatus(403); // Forbidden
+        req.user = decoded.username;
+        req.roles = decoded.roles;
+        next();
+    });
+};
 
 module.exports = verifyJWT;
